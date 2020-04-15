10affee9f77ee2e4809d68fd1564297bdc12f7f4
madharris3350 <60237202+madharris3350@users.noreply.github.com>
2020-04-15 12:47:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,64 @@
-#Your name goes here
-
-#Your algorithm should go here OR you should use comments throughout
-
-#Your code goes here
+#Madison Harris 
+#This GUI -- converts temps in fahrenheit to temps in celsius
 import tkinter
-import tkinter.messagebox as box 
+import tkinter.messagebox
 
-class FahrenConverterGUI:
+class FahrenheitConverterGUI:
     def __init__(self):
-      # Create the main window
+
+        #Create the main window
         self.main_window = tkinter.Tk()
 
-        # Create two frames to group widgets
+        #Create two frames to group widgets
         self.top_frame = tkinter.Frame(self.main_window)
         self.bottom_frame = tkinter.Frame(self.main_window)
 
-        #Create widgets for the top frame
+        #Create the widgets for the top frame.
+        #we need a prompt label and a entry box
+        self.prompt_label = tkinter.Label(self.top_frame,
+                                          text = 'Enter a temperature in Fahrenheit:')
+        self.fahrenheit_entry = tkinter.Entry(self.top_frame,
+                                       width = 10)
         
         #Pack the top frame's widgets
-        
-        #Create widgets for the bottom frame
+        self.prompt_label.pack(side = 'left')
+        self.fahrenheit_entry.pack(side = 'left')
+
+        #Create the button widgets for the bottom frame
+        self.calc_button = tkinter.Button(self.bottom_frame,
+                                          text = 'Convert',
+                                          command = self.convert)
         self.quit_button = tkinter.Button(self.bottom_frame,
-                                          text='Quit',
-                                          command=self.main_window.destroy)
-        
-        #Pack the bottom frame's widgets
-        self.quit_button.pack(side='left')
-      
-        # Pack the frames
+                                          text = 'Quit',
+                                          command = self.main_window.destroy)
+
+        #Pack the buttons
+        self.calc_button.pack(side = 'left')
+        self.quit_button.pack(side = 'left')
+
+        #Pack the frames
         self.top_frame.pack()
         self.bottom_frame.pack()
-        
-        
-temp_converter = FahrenConverterGUI()
+
+        #Enter the tkinter main loop
+        tkinter. mainloop()
+
+    #The convert method is a callback function for the Calculate button
+    def convert(self):
+        #Get the value entered by the user into the fahrenheit_entry widget
+        fahrenheit = float(self.fahrenheit_entry.get())
+
+        #Convert fahrenheit to celsius
+        celsius = ((fahrenheit -32) * 5) /9
+
+        #Display the results in an info dialog box
+        tkinter.messagebox.showinfo('Results',
+                                 str(fahrenheit) +
+                                 ' Fahrenheit is equal to ' +
+                                 str(celsius) + ' Celsius.')
+
+
+#Create an instance of the FahrenheitConverterGUI class
+fahrenheit_conv = FahrenheitConverterGUI()
+
+
